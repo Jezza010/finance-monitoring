a4ac9f7067b525ef6b5386f41e5817af2066d982
kievsan <kievsan@mail.ru>
2025-04-25 17:35:42 +0300

    add Docker compose

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,36 +11,32 @@ create schema if not exists storage authorization admin; --postgres;
 -- ------------------------------------------------------------------
 
 -- Справочник: Типы лиц
-CREATE TABLE person_types (
+CREATE TABLE IF NOT EXISTS storage.person_types (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Справочник: Типы транзакций
-CREATE TABLE transaction_types (
+CREATE TABLE IF NOT EXISTS storage.transaction_types (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Справочник: Статусы транзакций
-CREATE TABLE transaction_statuses (
+CREATE TABLE IF NOT EXISTS storage.transaction_statuses (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Справочник: Категории
-CREATE TABLE categories (
+CREATE TABLE IF NOT EXISTS storage.categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL
 );
 
 -- Основная таблица: Транзакции
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-    person_type_id INT NOT NULL REFERENCES person_types(id),
-    transaction_type_id INT NOT NULL REFERENCES transaction_types(id),
-    status_id INT NOT NULL REFERENCES transaction_statuses(id),
-    category_id INT REFERENCES categories(id),
+CREATE TABLE IF NOT EXISTS storage.transactions (
+    id BIGSERIAL PRIMARY KEY,
     transaction_datetime TIMESTAMP NOT NULL,
     comment TEXT,
     amount NUMERIC(15, 5) CHECK (amount >= 0),
@@ -49,7 +45,15 @@ CREATE TABLE transactions (
     account_number VARCHAR(34),
     receiver_account_number VARCHAR(34),
     receiver_inn VARCHAR(11) CHECK (receiver_inn ~ '^\d{11}$'),
-    receiver_phone VARCHAR(16) CHECK (receiver_phone ~ '^(\+7|8)\d{10}$')
+    receiver_phone VARCHAR(16) CHECK (receiver_phone ~ '^(\+7|8)\d{10}$'),
+    person_type_id INT NOT NULL,
+    transaction_type_id INT NOT NULL,
+    status_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (person_type_id) REFERENCES storage.person_types (id),
+    FOREIGN KEY (transaction_type_id) REFERENCES storage.transaction_types (id),
+    FOREIGN KEY (status_id) REFERENCES storage.transaction_statuses (id),
+    FOREIGN KEY (category_id) REFERENCES storage.categories (id)
 );
 
 
