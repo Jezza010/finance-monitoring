cb997dddf0ceab33a67c2ca58f85edd4e4687d2b
sjavastar-t <sjavastar@gmail.com>
2025-04-29 19:54:34 +0300

    API Transaction

--- src/main/java/com/finmonitor/model/jdbc/Transaction.java
+++ src/main/java/com/finmonitor/model/jdbc/Transaction.java
@@ -1,26 +1,63 @@
 package com.finmonitor.model.jdbc;
 
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.math.BigDecimal;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 @NoArgsConstructor
-public class Transaction {
+@Builder
+public class Transaction  implements Serializable {
+
     public Long id;
-    public String personType;
-    public String transactionType;
-    public String category;
-    public String dateTime;
+    public Date transaction_datetime;
+    public double sum;
     public String comment;
-    public BigDecimal amount;
-    public String status;
     public String senderBank;
-    public String receiverBank;
-    public String accountNumber;
-    public String receiverAccountNumber;
+    public String senderAccountNumber;
     public String receiverINN;
+    public String receiverBank;
+    public String receiverAccountNumber;
     public String receiverPhone;
+    public String personTypeId;
+    public String transactionTypeId;
+    public String transactionStatusId;
+    public String categoryId;
+    public Long userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(sum, that.sum) == 0 &&
+                Objects.equals(transaction_datetime, that.transaction_datetime) &&
+                Objects.equals(comment, that.comment) &&
+                Objects.equals(senderBank, that.senderBank) &&
+                Objects.equals(senderAccountNumber, that.senderAccountNumber) &&
+                Objects.equals(receiverINN, that.receiverINN)
+                && Objects.equals(receiverBank, that.receiverBank)
+                && Objects.equals(receiverAccountNumber, that.receiverAccountNumber)
+                && Objects.equals(receiverPhone, that.receiverPhone)
+                && Objects.equals(personTypeId, that.personTypeId)
+                && Objects.equals(transactionTypeId, that.transactionTypeId)
+                && Objects.equals(transactionStatusId, that.transactionStatusId)
+                && Objects.equals(categoryId, that.categoryId)
+                && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                transaction_datetime, sum,
+                comment,
+                senderBank, senderAccountNumber,
+                receiverINN, receiverBank, receiverAccountNumber, receiverPhone,
+                personTypeId, transactionTypeId, transactionStatusId, categoryId, userId);
+    }
 }
