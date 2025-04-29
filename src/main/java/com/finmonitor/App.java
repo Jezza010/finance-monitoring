119068357aef416593e917c6664fb67dbfc2e1b4
Jezza010 <mr.vudi36@gmail.com>
2025-04-29 21:11:01 +0300

    user и авторизация

--- src/main/java/com/finmonitor/App.java
+++ src/main/java/com/finmonitor/App.java
@@ -1,6 +1,7 @@
 package com.finmonitor;
 
 import com.finmonitor.config.JDBCConnector;
+import com.finmonitor.http.AuthHandler;               // обработчик авторизации
 import com.finmonitor.http.TransactionHandler;
 import com.sun.net.httpserver.HttpServer;
 
@@ -11,20 +12,29 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        //try (Connection conn = DatabaseConnector.connect()) {
         try (Connection conn = JDBCConnector.getConnection()) {
             System.out.println("Successful test connection to db!");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        //обработчик авторизации
+        AuthHandler authHandler = new AuthHandler();
+        server.createContext("/api/auth/register", authHandler);
+        server.createContext("/api/auth/login", authHandler);
+
+        // обработчик транзакций
         var handler = new TransactionHandler();
         server.createContext("/api/transaction", handler::transaction);
         server.createContext("/api/transactions_count", handler::transactionsCount);
         server.createContext("/api/debet_count", handler::debetCount);
         server.createContext("/api/sum_income", handler::sumIncome);
+
         server.setExecutor(null);
         server.start();
+
         System.out.println("The server is running on port 8080...");
     }
 }
