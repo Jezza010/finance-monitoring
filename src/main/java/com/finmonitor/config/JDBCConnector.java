c9ba1caa5eda99b0f49da09d9285413df5bf96ec
kievsan <kievsan@mail.ru>
2025-05-05 14:23:35 +0300

    logging

--- src/main/java/com/finmonitor/config/JDBCConnector.java
+++ src/main/java/com/finmonitor/config/JDBCConnector.java
@@ -5,8 +5,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
